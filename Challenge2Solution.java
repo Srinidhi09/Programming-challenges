89b672f09422bcc2fdd2d243ba7ee1132a01a58b
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2018-02-03 18:14:25 -0800

    Trying to decrease complexity of solution

--- Challenge2Solution.java
+++ Challenge2Solution.java
@@ -1,25 +1,32 @@
-import java.util.Scanner;
+/**
+ * Implement an algorithm to determine if a string has all unique characters or not
+ */
 
-class Challenge2Solution{
-    public static boolean checkUniqueChar( String s) {
-        for (int i = 1; i < s.length(); i++) {
-            for (int j = 0; j < i; j++)
-                //checking if character at index i matches with any character at index j
-                if (s.charAt(i) == s.charAt(j)) return false;
-        }
-        return true;
+import java.util.*;
+
+public class Challenge2Solution{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any String :");
+        String str = sc.nextLine();
+        string uc = new string();
+        boolean result = uc.checkUnique(str);
+        if(result)
+            System.out.println("String has all unique characters");
+        else
+            System.out.println("String does not have all unique characters");
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any unique String :");
-        String newString = sc.nextLine();
-        if (checkUniqueChar(newString)){
-            System.out.println("The String " +newString+ " has all unique values");
+    public boolean checkUnique(String str){
+
+        HashSet hashSet = new HashSet(str.length());
+
+        for(char c : str.toCharArray()){ //iterate through character array
+            if(!hashSet.add(Character.toUpperCase(c)))//try to add each char to hashset
+                return false; //return false if could not add
         }
-        else {
-            System.out.println("Oops, the String " +newString+ " has duplicate values!");
-        }
-        return;
+        return true;
     }
 }
