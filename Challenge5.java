aba31c6c208f385dda1636eb45ef8696af954eaf
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2018-02-23 13:29:26 -0800

    Checking for Palindromes

---
+++ Challenge5.java
@@ -0,0 +1,10 @@
+
+// Check for palindromes
+
+function palindrome(str) {
+  
+   return str.replace(/[\W_]/g, '').toLowerCase() === str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+   
+}
+
+palindrome("madam");
