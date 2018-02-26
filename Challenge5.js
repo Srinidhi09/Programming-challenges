5e89e21da901d984ad3f4e6464c992276878969a
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2018-02-26 13:29:40 -0800

    Checking for palindromes

--- Challenge5.js
+++ Challenge5.js
@@ -1,10 +1,6 @@
-
 // Check for palindromes
 
 function palindrome(str) {
-  
    return str.replace(/[\W_]/g, '').toLowerCase() === str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
-   
 }
-
 palindrome("madam");
