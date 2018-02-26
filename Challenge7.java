934c85d5132390abcbcbbf2859bd04089a6ef1e4
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2018-02-25 21:26:12 -0800

    Title Case a String

--- Challenge7.java
+++ Challenge7.java
@@ -1,14 +1,28 @@
 // Change first letter of each word to Capital in a string
 
 function titleCase(str) {
+
+    /* Solution 1
+    var toArray = str.toLowerCase().split(' ');
+
+    var result = toArray.map(function(text){
+      return text.replace(text.charAt(0), text.charAt(0).toUpperCase());
+    });
+
+    return result.join(' ');
+    */
+
+   // Solution 2
  
-  var toArray = str.toLowerCase().split(' ');
-   
-  var result = toArray.map(function(text){
-    return text.replace(text.charAt(0), text.charAt(0).toUpperCase());
-  });
-  
-  return result.join(' ');
+    let arr = str.toLowerCase().split(' ');
+    let result = [];
+
+    for(let x of arr){
+
+        let y = x.replace(x.charAt(0), x.charAt(0).toUpperCase());
+        result.push(y);
+    }
+    return result.join(' ');
 }
 
-titleCase("I'm a little tea pot");
+titleCase("sHoRt AnD sToUt");
